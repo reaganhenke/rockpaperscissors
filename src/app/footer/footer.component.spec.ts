6df2eda715c3a6435ea57ff3de0ec4483f8a42de
Reagan Henke <reaganhenke@gmail.com>
2024-01-13 13:43:34 -0500

    clean and test footer opening dialog

--- src/app/footer/footer.component.spec.ts
+++ src/app/footer/footer.component.spec.ts
@@ -1,17 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { FooterComponent } from './footer.component';
+import {
+  MatDialogModule,
+  MatDialog
+} from '@angular/material/dialog';
+import { AboutComponent } from '../about/about.component';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
 
+  const mockDialog = {
+    open: jasmine.createSpy('open')
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FooterComponent]
-    })
-    .compileComponents();
-    
+      imports: [FooterComponent, MatDialogModule],
+      providers: [
+        {
+          provide: MatDialog,
+          useValue: mockDialog
+        }
+      ]
+    }).compileComponents();
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +31,10 @@ describe('FooterComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should open dialog', () => {
+    component.openDialog();
+    expect(mockDialog.open).toHaveBeenCalledOnceWith(AboutComponent, {});
   });
 });
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,14 +1,6 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
-import {
-  MatDialog,
-  MAT_DIALOG_DATA,
-  MatDialogRef,
-  MatDialogTitle,
-  MatDialogContent,
-  MatDialogActions,
-  MatDialogClose,
-} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { AboutComponent } from '../about/about.component';
 
 @Component({
@@ -19,17 +11,9 @@ import { AboutComponent } from '../about/about.component';
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
-  constructor(public dialog: MatDialog) {}
+  constructor(public dialog: MatDialog) { }
 
   openDialog(): void {
-    const dialogRef = this.dialog.open(AboutComponent, {
-      // data: {name: this.name, animal: this.animal},
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log('The dialog was closed');
-      // this.animal = result;
-    });
+    this.dialog.open(AboutComponent, {});
   }
-  
 }
